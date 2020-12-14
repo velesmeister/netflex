bf838349b74c8c57f5b915acb29182be6d5b60ec
Egorka <velesmeister@gmail.com>
2020-12-14 06:34:31 +0500

    UI in progress

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const io = require('socket.io')
 const port = 3000;
 
 const app = express();
 const server = http.createServer(app);
+const io = require('socket.io')(server)
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', ((req, res, next) => {
     res.status(200);
-    res.sendFile(path.resolve('views', 'index'));
+    res.sendFile(path.resolve('views', 'index.html'));
+}))
+
+app.get('/task', ((req, res, next) => {
+    res.status(200);
+    res.sendFile(path.resolve('views', 'task.html'));
 }))
 
 // Тут клиент пытается создать с нами стабильное соедениние
