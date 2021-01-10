1a2eb72c194ff92d1b372a7013b4b4ff32bd7bc6
zhdanushka <gopotron@list.ru>
2021-01-10 21:10:18 +0500

    some additional files

--- views/js/Router/auth.js
+++ views/js/Router/auth.js
@@ -12,15 +12,11 @@ router.post('/', async (req, res) => {
     if (error) {
         return res.status(400).send(error.details[0].message);
     }
-
-    //  Now find the user by their email address
     let user = await User.findOne({ email: req.body.email });
     if (!user) {
         return res.status(400).send('Incorrect email or password.');
     }
 
-    // Then validate the Credentials in MongoDB match
-    // those provided in the request
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) {
         return res.status(400).send('Incorrect email or password.');
